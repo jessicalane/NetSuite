4cae12dfb32c9cb6c8686393575e2d21bd734005
Jessica Lane <jessica.lynn.brasher@gmail.com>
2021-04-23 16:25:16 -0400

    updates to UE and CS bids scripts

--- CRM/CRM_CS_Bids.js
+++ CRM/CRM_CS_Bids.js
@@ -3,58 +3,59 @@
  *@NScriptType ClientScript
  *@CreatedBy Jessica Lane | 2021
  */
+
 define([], function() {
 
     function pageInit(context) {
-        
+
     }
 
-    function saveRecord(context) {
+    // function saveRecord(context) {
         
-    }
+    // }
 
-    function validateField(context) {
+    // function validateField(context) {
         
-    }
+    // }
 
-    function fieldChanged(context) {
+    // function fieldChanged(context) {
         
-    }
+    // }
 
-    function postSourcing(context) {
+    // function postSourcing(context) {
         
-    }
+    // }
 
-    function lineInit(context) {
+    // function lineInit(context) {
         
-    }
+    // }
 
-    function validateDelete(context) {
+    // function validateDelete(context) {
         
-    }
+    // }
 
-    function validateInsert(context) {
+    // function validateInsert(context) {
         
-    }
+    // }
 
-    function validateLine(context) {
+    // function validateLine(context) {
         
-    }
+    // }
 
-    function sublistChanged(context) {
+    // function sublistChanged(context) {
         
-    }
+    // }
 
     return {
-        pageInit: pageInit,
-        saveRecord: saveRecord,
-        validateField: validateField,
-        fieldChanged: fieldChanged,
-        postSourcing: postSourcing,
-        lineInit: lineInit,
-        validateDelete: validateDelete,
-        validateInsert: validateInsert,
-        validateLine: validateLine,
-        sublistChanged: sublistChanged
+        pageInit: pageInit
+        // saveRecord: saveRecord,
+        // validateField: validateField,
+        // fieldChanged: fieldChanged,
+        // postSourcing: postSourcing,
+        // lineInit: lineInit,
+        // validateDelete: validateDelete,
+        // validateInsert: validateInsert,
+        // validateLine: validateLine,
+        // sublistChanged: sublistChanged
     }
 });
--- CRM/CRM_UE_Bids.js
+++ CRM/CRM_UE_Bids.js
@@ -3,23 +3,32 @@
  *@NScriptType UserEventScript
  *@CreatedBy Jessica Lane | 2021
  */
-define([], function() {
+define(['N/record', 'N/ui/serverWidget'], function(record, serverWidget) {
 
     function beforeLoad(context) {
-        
+        var rec = context.newRecord;
+        var sub = rec.getSublist({
+            sublistId: 'item'
+        });
+        var column = sub.getSublistField({
+            id: 'amount'
+        });
+        column.updateDisplayType({
+            displayType: serverWidget.FieldDisplayType.HIDDEN
+        })
     }
 
-    function beforeSubmit(context) {
+    // function beforeSubmit(context) {
         
-    }
+    // }
 
-    function afterSubmit(context) {
+    // function afterSubmit(context) {
         
-    }
+    // }
 
     return {
-        beforeLoad: beforeLoad,
-        beforeSubmit: beforeSubmit,
-        afterSubmit: afterSubmit
+        beforeLoad: beforeLoad
+        // beforeSubmit: beforeSubmit,
+        // afterSubmit: afterSubmit
     }
 });
