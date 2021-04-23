b08b12adcda541346df8ffe14f2fffb1acaa5d20
Jessica Lane <jessica.lynn.brasher@gmail.com>
2021-04-23 15:07:27 -0400

    created Bids UE and CS script outlines

---
+++ CRM/CRM_UE_Bids.js
@@ -0,0 +1,25 @@
+/**
+ *@NApiVersion 2.0
+ *@NScriptType UserEventScript
+ *@CreatedBy Jessica Lane | 2021
+ */
+define([], function() {
+
+    function beforeLoad(context) {
+        
+    }
+
+    function beforeSubmit(context) {
+        
+    }
+
+    function afterSubmit(context) {
+        
+    }
+
+    return {
+        beforeLoad: beforeLoad,
+        beforeSubmit: beforeSubmit,
+        afterSubmit: afterSubmit
+    }
+});
