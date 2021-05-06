245bad9e5fb040118ad38bbaba7d96ec0c188a83
Jessica Lane <jessica.lynn.brasher@gmail.com>
2021-05-06 11:23:20 -0400

    created to-do list for gs1 generator

---
+++ Logistics/gs1-generator.js
@@ -0,0 +1,23 @@
+/**
+ *@NApiVersion 2.0
+ *@NScriptType UserEventScript
+ *@CreatedBy Jessica Lane | 2021
+ */
+define(['N/search', 'N/record'], function(search, record) {
+
+
+    function beforeSubmit(context) {
+        var rec = context.newRecord;
+
+        //TODO If deleting, exit script;
+
+        //TODO search customer record to see if SSCC Labels are needed
+        
+        //TODO If SSCC labels are needed, generate SSCC. Store last number in a Last Used Number custom record (SSCC Serial Reference field).
+
+    }
+
+    return {
+        beforeSubmit: beforeSubmit
+    }
+});
