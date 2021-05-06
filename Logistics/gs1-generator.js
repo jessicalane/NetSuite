975a5a087fd76f12e6584a84584c11de6a74d0c1
Jessica Lane <jessica.lynn.brasher@gmail.com>
2021-05-06 12:42:51 -0400

    work on gs1 generator

--- Logistics/gs1-generator.js
+++ Logistics/gs1-generator.js
@@ -20,9 +20,22 @@ define(['N/search', 'N/record'], function(search, record) {
             columns: [
                 search.createColumn({name: 'custentity_sscclabelsneeded', label: 'SSCC Labels Needed'})
             ]
-        }).run().each(function (result) {
+        }).run().each(function(result) {
             //TODO If SSCC labels are needed, generate SSCC. Store last number in a Last Used Number custom record (SSCC Serial Reference field).
+            if (result.getValue('custentity_sscclabelsneeded') == true) {
+                //TODO create search to find last number used for SSCC
+                search.create({
+                    type: 'customrecord_lastnumberused',
+                    filters: [],
+                    columns: [
+                        search.createColumn({name: 'internalid', summary: 'MAX'}),
+                        //is following data necessary?
+                        search.createColumn({name: 'custrecord_ssccserialreference', summary: 'MAX'})
+                    ]
+                }).run().each(function(result) {
 
+                })
+            }
         })
         
 
