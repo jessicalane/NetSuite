f027657fe94f6e33f5561dc3ee370d47d956e003
Jessica Lane <jessica.lynn.brasher@gmail.com>
2021-05-06 11:31:35 -0400

    finished todo for search

--- Logistics/gs1-generator.js
+++ Logistics/gs1-generator.js
@@ -8,12 +8,23 @@ define(['N/search', 'N/record'], function(search, record) {
 
     function beforeSubmit(context) {
         var rec = context.newRecord;
+        var customer = rec.getValue('entity');
 
         if (context.type == context.UserEventType.DELETE) {return;}
 
-        //TODO search customer record to see if SSCC Labels are needed
+        search.create({
+            type: 'customer',
+            filters: [
+                ['internalid', 'anyof', customer]
+            ],
+            columns: [
+                search.createColumn({name: 'custentity_sscclabelsneeded', label: 'SSCC Labels Needed'})
+            ]
+        }).run().each(function (result) {
+            //TODO If SSCC labels are needed, generate SSCC. Store last number in a Last Used Number custom record (SSCC Serial Reference field).
+
+        })
         
-        //TODO If SSCC labels are needed, generate SSCC. Store last number in a Last Used Number custom record (SSCC Serial Reference field).
 
     }
 
