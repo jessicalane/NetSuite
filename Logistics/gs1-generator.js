fe632426c79d81b76bcb37adb05401160dcaa433
Jessica Lane <jessica.lynn.brasher@gmail.com>
2021-05-10 12:36:58 -0400

    revamping gs1 generator

--- Logistics/gs1-generator.js
+++ Logistics/gs1-generator.js
@@ -24,49 +24,7 @@ define(['N/search', 'N/record'], function(search, record) {
         if (customerLookUp.custentity_sscclabelsneeded == true) {
 
             var pallets = rec.getValue('custbody_total_pallets_rounded');
-
-            var ssccSerialLookUp = search.lookupFields({
-                type: 'customrecord_script_lookups',
-                id: 5, //5 is the internalid of sscc serial
-                columns: ['custrecord_fft_field']
-            });
-
-            if (pallets == 1) {
-
-                var lastSerial = ssccSerialLookUp.custrecord_fft_field;
-                var newSerial = (Number(lastSerial) + 1).toString().padStart(7, '0');
-                log.debug('lastSerial1', lastSerial);
-                log.debug('newSerial1', newSerial);
-
-                rec.setValue({
-                    fieldId: 'custbody_gs1128numbers',
-                    value: newSerial
-                });
-                log.debug('lastSerial2', lastSerial);
-                log.debug('newSerial2', newSerial);
-
-                var ssccSerialLoad = record.load({
-                    type: 'customrecord_script_lookups',
-                    id: 5,
-                    isDynamic: true
-                });
-                log.debug('lastSerial3', lastSerial);
-                log.debug('newSerial3', newSerial);
-
-                ssccSerialLoad.setValue({
-                    fieldId: 'custrecord_fft_field',
-                    value: newSerial
-                });
-                log.debug('lastSerial4', lastSerial);
-                log.debug('newSerial4', newSerial);
-
-                ssccSerialLoad.save();
-                log.debug('lastSerial5', lastSerial);
-                log.debug('newSerial5', newSerial);
-
-            }
-
-
+            var itemLines = 
 
         }
 
