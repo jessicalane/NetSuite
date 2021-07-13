e3c15d3cfd0e298b6966990bcda28f76f95b5470
Jessica Lane <jessica.lynn.brasher@gmail.com>
2021-07-13 08:49:40 -0400

    search now gathering all values and setting all only on record create

--- CRM/CRM_CS_BidPricing.js
+++ CRM/CRM_CS_BidPricing.js
@@ -5,61 +5,72 @@
 define(['N/currentRecord', 'N/search'], function(currentRecord, search) {
 
     function pageInit(context) {
-        
+
         var rec = context.currentRecord;
         var opportunity = rec.getValue('custrecord_bid_opportunity');
         var uniqueId = rec.getValue('custrecord_unique_id');
         var lineId = uniqueId.split('-')[1];
+        var vendor = rec.getValue('custrecord_bidvendor');
 
-        search.create({
-            type: 'transaction',
-            filters: [
-                ['type', 'anyof', 'Opprtnty'],
-                'AND',
-                ['internalid', 'anyof', opportunity],
-                'AND',
-                ['mainline', 'is', 'F'],
-                'AND',
-                ['lineuniquekey', 'equalto', lineId]
-            ],
-            columns: [
-                'custcol_productsize', //Product Size
-                'custcol_pack_size', //Pack Size
-                'custcol_targetpricepoint', //Target Price Point
-                'custcol_productgrade', //Grade
-                'custcol_countryoforigin', //Country of Origin
-                'custcol_datingguidelinesrestrictions', //Dating Guidelines/Restrictions
-                'custcol_shelfliferequirements', //Shelf Life Requirements
-                'custcol_nutritionalrequirements', //Nutritional Requirements
-                'custcol_religiousrequirements', //Religious Requirements
-                'custcol_labelingrequirements', //Labeling Requirements
-                'custcol_vendoroptions' //Vendor Options
-            ]
-        }).run().each(function(result) {
+        //If the record is new, then it will pull the values and set them on the Bid Pricing record.
+        if (!vendor) {
 
-            //Get each value
-            var prodSize = result.getValue('custcol_productsize');
-            var packSize = result.getValue('custcol_pack_size');
-            var targetPrice = result.getValue('custcol_targetpricepoint');
-            var grade = result.getValue('custcol_productgrade');
-            var countryOrig = result.getValue('custcol_countryoforigin');
-            var datingGuide = result.getValue('custcol_datingguidelinesrestrictions');
-            var shelfLife = result.getValue('custcol_shelfliferequirements');
-            var nutritional = result.getValue('custcol_nutritionalrequirements');
-            var religious = result.getValue('custcol_religiousrequirements');
-            var labels = result.getValue('custcol_labelingrequirements');
-            var vendorOptions = result.getValue('custcol_vendoroptions');
+            search.create({
+                type: 'transaction',
+                filters: [
+                    ['type', 'anyof', 'Opprtnty'],
+                    'AND',
+                    ['internalid', 'anyof', opportunity],
+                    'AND',
+                    ['mainline', 'is', 'F'],
+                    'AND',
+                    ['lineuniquekey', 'equalto', lineId]
+                ],
+                columns: [
+                    'custcol_productsize', //Product Size
+                    'custcol_pack_size', //Pack Size
+                    'custcol_targetpricepoint', //Target Price Point
+                    'custcol_productgrade', //Grade
+                    'custcol_countryoforigin', //Country of Origin
+                    'custcol_datingguidelinesrestrictions', //Dating Guidelines/Restrictions
+                    'custcol_shelfliferequirements', //Shelf Life Requirements
+                    'custcol_nutritionalrequirements', //Nutritional Requirements
+                    'custcol_religiousrequirements', //Religious Requirements
+                    'custcol_labelingrequirements', //Labeling Requirements
+                    'custcol_vendoroptions' //Vendor Options
+                ]
+            }).run().each(function(result) {
 
-            //Set each value
-            //TODO Only set these fields if a new bid, not an existing.
+                //Get each value
+                var prodSize = result.getValue('custcol_productsize');
+                var packSize = result.getValue('custcol_pack_size');
+                var targetPrice = result.getValue('custcol_targetpricepoint');
+                var grade = result.getValue('custcol_productgrade');
+                var countryOrig = result.getValue('custcol_countryoforigin');
+                var datingGuide = result.getValue('custcol_datingguidelinesrestrictions');
+                var shelfLife = result.getValue('custcol_shelfliferequirements');
+                var nutritional = result.getValue('custcol_nutritionalrequirements');
+                var religious = result.getValue('custcol_religiousrequirements');
+                var labels = result.getValue('custcol_labelingrequirements');
+                var vendorOptions = result.getValue('custcol_vendoroptions');
+                
+                //Set each value
+                rec.setValue('custrecord_productsize', prodSize);
+                rec.setValue('custrecord_pack', packSize);
+                rec.setValue('custrecord_targetpricepoint', targetPrice);
+                rec.setValue('custrecord_grade', grade);
+                rec.setValue('custrecord_countryoforigin', countryOrig);
+                rec.setValue('custrecord_datingguidelinesrestrictions', datingGuide);
+                rec.setValue('custrecord_shelfliferequirements', shelfLife);
+                rec.setValue('custrecord_nutritionalrequirements', nutritional);
+                rec.setValue('custrecord_religiousrequirements', religious);
+                rec.setValue('labelrequirements', labels);
+                rec.setValue('custrecord_vendoroptions', vendorOptions);
+            });
 
-            rec.setValue('custrecord_productsize', prodSize);
-            rec.setValue('custrecord_pack', packSize);
-            //TODO Create target price point field and set value here.
-            rec.setValue('custrecord_grade', grade);
-            rec.setValue('custrecord_countryoforigin', countryOrig);
-            rec.setValue('')
-        });  
+        }
+        
+          
         
 
     }
